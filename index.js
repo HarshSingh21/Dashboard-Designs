4040bf3a436b14573826607d4783e694a6d1e5a7
Harsh Singh <singhharsh8283@gmail.com>
2023-09-14 17:25:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
 sideLinks.forEach(item => {
+    
     const li = item.parentElement;
     item.addEventListener('click', () => {
         sideLinks.forEach(i => {
